a42a5b16bf54f900982c15b4fa92f3442229ae5e
ClarkG10 <your.email@example.com>
2024-04-04 10:20:44 +0800

    added design to html and css

--- js/dashboard/index.js
+++ js/dashboard/index.js
@@ -5,6 +5,23 @@ import {backendURL} from "../utils/utils.js";
         document.querySelector(".logout").classList.add("d-block");
         document.querySelector(".login1").classList.remove("d-block");
         document.querySelector(".login1").classList.add("d-none");
+
+        const response = await fetch(backendURL + "/api/profile/show", { 
+            headers: {
+                Accept: "application/json",
+                Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+        }); 
+
+        if(response.ok){
+        const json = await response.json();
+        console.log(json);
+        document.getElementById("user_logged").innerHTML = json.name;
+        }else{
+            const json = await response.json();
+            document.querySelector(".wrongbutton").click();
+            document.querySelector("#wrongModal .wrong").innerHTML = json.message;
+        }
     } else {
         document.querySelector(".logout").classList.remove("d-block");
         document.querySelector(".logout").classList.add("d-none");
